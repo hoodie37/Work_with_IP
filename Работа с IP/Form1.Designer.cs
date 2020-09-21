b88f3b2a9b5ae048e58178b99ef20c1b9b329218
Василий Яковлев <vasya_anime@mail.ru>
2020-09-21 16:04:55 +0500

    Добавьте файлы проекта.

---
+++ Работа с IP/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Работа_с_IP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.textBIP = new System.Windows.Forms.TextBox();
+            this.textBMask = new System.Windows.Forms.TextBox();
+            this.textBrIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(94, 141);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(351, 211);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP";
+            this.columnHeader1.Width = 129;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "DNS";
+            this.columnHeader2.Width = 116;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Ping";
+            this.columnHeader3.Width = 129;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(194, 85);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "Scan";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(94, 29);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 20);
+            this.tb1.TabIndex = 7;
+            this.tb1.Text = "193.233.146.226";
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(267, 29);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(100, 20);
+            this.tb2.TabIndex = 8;
+            this.tb2.Text = "193.233.146.229";
+            // 
+            // textBIP
+            // 
+            this.textBIP.Location = new System.Drawing.Point(554, 156);
+            this.textBIP.Name = "textBIP";
+            this.textBIP.Size = new System.Drawing.Size(169, 20);
+            this.textBIP.TabIndex = 9;
+            // 
+            // textBMask
+            // 
+            this.textBMask.Location = new System.Drawing.Point(554, 200);
+            this.textBMask.Name = "textBMask";
+            this.textBMask.Size = new System.Drawing.Size(169, 20);
+            this.textBMask.TabIndex = 10;
+            // 
+            // textBrIP
+            // 
+            this.textBrIP.Location = new System.Drawing.Point(554, 243);
+            this.textBrIP.Name = "textBrIP";
+            this.textBrIP.Size = new System.Drawing.Size(169, 20);
+            this.textBrIP.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(554, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "IP address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(554, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Mask:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(554, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Broadcast IP address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(554, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "MAC:";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(554, 292);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(169, 95);
+            this.listBox.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBrIP);
+            this.Controls.Add(this.textBMask);
+            this.Controls.Add(this.textBIP);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox textBIP;
+        private System.Windows.Forms.TextBox textBMask;
+        private System.Windows.Forms.TextBox textBrIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ Работа с IP/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Работа_с_IP
+{
+    public partial class Form1 : Form
+    {
+       
+        public bool Connect(IPAddress ip)
+        {
+            Ping Pinger = new Ping();                 //вот это вот утилита ping которую он скинул на почту
+            PingReply pingrep = Pinger.Send(ip, 100);
+
+            if (pingrep.Status == IPStatus.Success) //если мы можем достучаться до этого адреса то true, иначе false
+                return true;
+            else return false;
+
+
+        }
+
+
+
+        public List<IPAddress> GetList(IPAddress beginIP, IPAddress endIP)
+        {
+            List<IPAddress> list = new List<IPAddress>(); // создаем объект лист класса List<IPAddress>
+            var beginIParray = beginIP.GetAddressBytes(); // массив байтов
+            var endIParray = endIP.GetAddressBytes();
+            Array.Reverse(beginIParray); //изменяем порядок жлементов на обратный
+            Array.Reverse(endIParray);
+            var beginIPint = BitConverter.ToInt32(beginIParray, 0); //преобразуем в число
+            var endIPint = BitConverter.ToInt32(endIParray, 0);
+            for (; beginIPint <= endIPint; beginIPint++)
+            {
+                var IParray = BitConverter.GetBytes(beginIPint); //возвращает число как массив байтов
+                Array.Reverse(IParray); //меняем обратно
+                list.Add(new IPAddress(IParray)); //заносим в лист
+            };
+            return list;
+        }
+
+
+        void GetMac()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            string mac = "";
+            foreach (NetworkInterface adapter in nics)
+            {
+                PhysicalAddress address = adapter.GetPhysicalAddress(); //этот кусок кода я нагло взял из интернета, тут надо знать команду ГетФизикалАдрес
+                byte[] bytes = address.GetAddressBytes();
+                for (int k = 0; k < bytes.Length; k++)
+                {
+                    mac += bytes[k].ToString("X2");
+                    if (k != bytes.Length - 1)
+                    {
+                        mac += "-";
+                    }
+                }
+                listBox.Items.Add(mac);
+                mac = "";
+
+            }
+
+        }
+
+
+        void info(IPAddress beginIP, IPAddress endIP)
+        {
+            var beginIParray = beginIP.GetAddressBytes(); // массив байтов
+            var endIParray = endIP.GetAddressBytes();
+            byte[] maskarray = new byte[4];             //новые массивы
+            byte[] ipadressarray = new byte[4];
+            byte[] broadcastiparray = new byte[4];
+
+            bool end1 = false;
+            for (int i = 0; i < 4; i++)
+            {
+                for (byte bit = 128; bit >= 1; bit /= 2)
+                {
+                    if (!end1 && (beginIParray[i] & bit) == (endIParray[i] & bit)) //если end1 стоит в позиции ложь и биты начала и конца совпадают, то
+                    {
+                        maskarray[i] |= bit; //бит уходит в маску
+                    }
+                    else
+                    {
+                        end1 = true;
+                        maskarray[i] = (byte)(maskarray[i] & ~bit); //иначе он меняется
+                    }
+                }
+                ipadressarray[i] = (byte)(maskarray[i] & beginIParray[i]);      //побитовая конъюнкция маски и первого айпишника(хотя можно брать любой айпишник из диапазона
+                broadcastiparray[i] = (byte)(~maskarray[i] | ipadressarray[i]); //побитовая дизъюнкция инвертированной маски и айпишника сети
+            }
+
+
+            IPAddress ipAddress = new IPAddress(ipadressarray); //записываем всё в текст боксы
+            textBIP.Text = ipAddress.ToString();
+
+            IPAddress mask = new IPAddress(maskarray);
+            textBMask.Text = mask.ToString();
+
+            IPAddress broadcast = new IPAddress(broadcastiparray);
+            textBrIP.Text = broadcast.ToString();
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string IP1 = tb1.Text;  //Берем IPшники из текстбоксов
+            string IP2 = tb2.Text;
+            IPAddress beginIP = IPAddress.Parse(IP1); //парсим айпишники в класс IPAddress
+            IPAddress endIP = IPAddress.Parse(IP2);
+
+            List<IPAddress> list = GetList(beginIP, endIP);
+            info(beginIP, endIP);
+            GetMac();
+
+            foreach (IPAddress ip in list)
+            {
+
+                Connect(ip);
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = ip.ToString();
+                string host = Dns.GetHostEntry(ip).HostName.ToString(); //днс этого айпишника
+                lvi.SubItems.Add(host);
+                if (Connect(ip) == true)
+                    lvi.SubItems.Add("true");
+                else lvi.SubItems.Add("false");
+                listView1.Items.Add(lvi);
+            }
+
+
+
+        }
+    }
+}
